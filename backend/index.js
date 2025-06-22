e7af7733e3f66df2d1f0e41f72921c90a1c2a268
Sandhya Sisodiya <sandhyasisodiya1033@gmail.com>
2025-06-22 17:19:37 +0530

    change in cors origin path

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,9 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:['http://localhost:5173',
+        "https://gmail-clone-flax.vercel.app"
+    ],
     credentials:true
 }
 app.use(cors(corsOptions));
